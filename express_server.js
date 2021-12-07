cb8fc284a28611517d74b20cad72b43f902898ce
gmarkoski <gary_markoski@hotmail.com>
2021-12-06 20:43:57 -0700

    updating with random generator

--- express_server.js
+++ express_server.js
@@ -9,9 +9,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const generateRandomString = function(length = 6) {
+  return Math.random().toString(36).substr(2, length);
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -19,6 +26,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
