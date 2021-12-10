d28446b531d85ac166a6bc900e36347a4855e3a5
gmarkoski <gary_markoski@hotmail.com>
2021-12-10 15:54:23 -0700

    with helpersTest

---
+++ test/helpersTest.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.isTrue(user.id === expectedUserID);
+  });
+  it('should return undefined for non-user', function() {
+    const user = findUserByEmail("bill@juice.ca", testUsers);
+    assert.isNull(user);
+  });
+});
